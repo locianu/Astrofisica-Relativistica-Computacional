01f95891f1c1921f752bc22e89848468e0c0aa7f
Luciano <114966586+locianu@users.noreply.github.com>
2023-09-18 22:55:39 -0300

    Add files via upload

--- cod_num_geodesicas_luz.py
+++ cod_num_geodesicas_luz.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 from mpl_toolkits.mplot3d import Axes3D
 
 
@@ -12,6 +13,20 @@ R = 2*G*M
 radius_celestial_sphere = 80
 aDiskMin = 2*R
 aDiskMAx = 5*R
+
+# funções matemáticas
+
+def sin(theta):
+     return math.sin(theta)
+
+def cos(theta):
+     return math.cos(theta)
+
+def csc(theta):
+     return 1 / math.sin(theta)
+
+def sqrt(theta):
+     return math.sqrt(theta)
 
 # funções da métrica
 
@@ -73,7 +88,11 @@ coords_aDisk = np.zeros((resolution_height,resolution_width, 3))
 # passo do runge-kunta
 
 #stepsize = 0.1
-h = 0.1
+sts = 0.1
+
+#definindo pi para facilitar a vida
+
+pi = math.pi
 
 # sinceramente eu não entendi essa parte do programa
 
@@ -83,4 +102,25 @@ for j in range(1,2):
              w = -window_width / 2 + (j - 1) * window_width / (resolution_width - 1)
 
              r = 70
-             theta = math.pi             
+             theta = pi / 2 - pi / 46 # deslocando o buraco negro para visualização com o aDisk (disco de acreção?)
+             phi = 0
+             t_dot = 1
+
+             phi_dot = ((csc(theta) * w) / 
+                        (sqrt((a**2 + r**2) * distance_from_window**2 + w**2 
+                              + h**2)))
+             p_r = (2 * Sigma(r, theta) * (h * (a**2 + r**2) * cos(theta) + r)
+                     + r * sqrt(a**2 + r**2) * sin(theta) * distance_from_window 
+                     / (sqrt(distance_from_window**2 + h**2 + w**2) * (a**2 +2 
+                     * r**2 + a**2 * cos(2 * theta)) * Delta(r)))
+             p_theta = (2 * Sigma(r, theta) * 
+                        (-h * r * sin(theta) + sin(theta) + sqrt(a**2 + r**2)
+                         * cos(theta) * distance_from_window)
+                         / (sqrt(distance_from_window**2 + h**2 + w**2) 
+                            * (a**2 + 2*r**2 + a**2 * cos(2*theta))))
+             E = (1 - R / r) * t_dot + (R * a * phi_dot) / r
+             L = (-(R * a) / r * t_dot 
+                  + (r**2 + a**2 + (R * a**2) / r) * phi_dot)
+             
+             # geodésicas
+             
